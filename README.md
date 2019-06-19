4700330163d890a5120d75a7f2d1a921f7cd558b
hackercgh <hacker_cgh@163.com>
2019-06-19 14:38:13 +0800

    aaa

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # goLearn
+** GoLang 学习 **
--- src/apps/pianyitest.go
+++ src/apps/pianyitest.go
@@ -1,10 +1,37 @@
 package main
 
-import "lib/publib/github.com/wonderivan/logger"
+import (
+	"fmt"
+	"io/ioutil"
+	"lib/publib/github.com/wonderivan/logger"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 
 	logger.Debug("8 << 1 = %v",8 << 1)
 	logger.Debug("8 >> 1 = %v",8 >> 1)
+	ExampleFileServer()
 
+}
+
+func ExampleGet() {
+	res, err := http.Get("https://www.jianshu.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", robots)
+}
+
+func ExampleFileServer() {
+	// Simple static webserver:
+	dir := fmt.Sprintf("%s/file",os.Getenv("GOPATH"))
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir(dir))))
 }
